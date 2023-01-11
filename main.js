8b4e5b7188d13bda38aa0dc9820439bfa2e90596
Felix <felix.kuhbier@yahoo.com>
2023-01-12 00:49:13 +0100

    initial commit

---
+++ main.js
@@ -0,0 +1,26 @@
+
+
+const { token } = require('./config.json');
+const { Client, Events, GatewayIntentBits } = require('discord.js');
+
+const client = new Client({
+	intents: [GatewayIntentBits.Guilds, GatewayIntentBits.GuildMessages, GatewayIntentBits.GuildMessageReactions],
+});
+
+client.once(Events.ClientReady, c => {
+	console.log(`Ready! Logged in as ${c.user.tag}`);
+});
+
+client.on(Events.MessageCreate, async (message) => {
+	const date = new Date();
+	
+		if(date.getDate == message.createdAt.getDate){
+			console.log((await message.author.send(message.author.avatarURL())));
+			
+		}
+		
+	
+
+});
+
+client.login(token);

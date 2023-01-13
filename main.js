d77c332d2f1b3821a0b6cdd8d80e02f908c6e67e
Felix <felix.kuhbier@yahoo.com>
2023-01-13 13:30:19 +0100

    optimize event and command handling

--- main.js
+++ main.js
@@ -1,23 +1,42 @@
-const { token } = require('./config.json');
-const { Client, Events, GatewayIntentBits } = require('discord.js');
+const fs = require('node:fs');
+const path = require('node:path');
+const dotenv = require('dotenv');
+const { Client, Events, GatewayIntentBits, Collection } = require('discord.js');
 
-const client = new Client({
-	intents: [GatewayIntentBits.Guilds, GatewayIntentBits.GuildMessages, GatewayIntentBits.GuildMessageReactions],
-});
+dotenv.config();
 
-client.once(Events.ClientReady, c => {
-	console.log(`Ready! Logged in as ${c.user.tag}`);
-});
+// Create a new client instance
+const client = new Client({ intents: [GatewayIntentBits.Guilds] });
+client.commands = new Collection();
 
-client.on(Events.MessageCreate, async (message) => {
-	const date = new Date();
+const eventsPath = path.join(__dirname, 'events');
+const eventFiles = fs.readdirSync(eventsPath).filter(file => file.endsWith('.js'));
 
-	if (date.getDate == message.createdAt.getDate) {
-		console.log((await message.author.send(message.author.avatarURL())));
-
+for (const file of eventFiles) {
+	const filePath = path.join(eventsPath, file);
+	const event = require(filePath);
+	if (event.once) {
+		client.once(event.name, (...args) => event.execute(...args));
 	}
+	else {
+		client.on(event.name, (...args) => event.execute(...args));
+	}
+}
+
+const commandsPath = path.join(__dirname, 'commands');
+const commandFiles = fs.readdirSync(commandsPath).filter(file => file.endsWith('.js'));
+
+for (const file of commandFiles) {
+	const filePath = path.join(commandsPath, file);
+	const command = require(filePath);
+	// Set a new item in the Collection with the key as the command name and the value as the exported module
+	if ('data' in command && 'execute' in command) {
+		client.commands.set(command.data.name, command);
+	}
+	else {
+		console.log(`[WARNING] The command at ${filePath} is missing a required "data" or "execute" property.`);
+	}
+}
 
 
-});
-
-client.login(token);
+client.login(process.env.DISCORD_TOKEN);

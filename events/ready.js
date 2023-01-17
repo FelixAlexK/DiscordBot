cd2281a045cab13833cdf7cbb5dfc458c3afe38e
Felix <felix.kuhbier@yahoo.com>
2023-01-17 21:07:45 +0100

    added activity type

--- events/ready.js
+++ events/ready.js
@@ -1,5 +1,5 @@
 const { Events } = require('discord.js');
-const { botName, botAvatar, botAtivityStatus, botStatus } = require('../config.json');
+const { botName, botAvatar, botAtivityStatus, botStatus, botActivityType } = require('../config.json');
 module.exports = {
 	name: Events.ClientReady,
 	once: true,
@@ -7,7 +7,7 @@ module.exports = {
 		await client.user.setUsername(botName);
 		await client.user.setAvatar(botAvatar);
 
-		await client.user.setActivity(botAtivityStatus);
+		await client.user.setActivity(botAtivityStatus, { type: `${botActivityType}` });
 		await client.user.setStatus(botStatus);
 
 		console.log(`Ready! Logged in as ${client.user.tag}`);

60c4911f7f720552bf4d18622c37855aab6b98a2
Felix <felix.kuhbier@yahoo.com>
2023-01-18 01:12:00 +0100

    deleted event

--- events/error.js
+++
@@ -1,8 +0,0 @@
-const { Events } = require('discord.js');
-
-module.exports = {
-	name: Events.Error,
-	async execute(interaction) {
-		interaction.reply(`client's WebSocket encountered a connection error: ${interaction}`);
-	},
-};

7b161ceb554158a902e6d989fdb597067b4b88b9
Felix <felix.kuhbier@yahoo.com>
2023-01-20 00:57:05 +0100

    optimize exception handling

--- commands/chuckNorrisJokes.js
+++ commands/chuckNorrisJokes.js
@@ -1,36 +1,34 @@
-const { SlashCommandBuilder} = require('discord.js');
+const { SlashCommandBuilder } = require('discord.js');
 const helper = require('../helper.js');
-
+const logger = require('../logger.js');
 module.exports = {
 	data: new SlashCommandBuilder()
 		.setName('chucknorris')
 		.setDescription('returns a random Chuck Norris joke'),
 	async execute(interaction) {
 
-		fetch('https://api.chucknorris.io/jokes/random')
-			.then(response => response.json())
-			.then(data => {
 
-				if (data.value) {
-					const exampleEmbed = {
-						color: helper.getRandomColor(),
-						title: 'Chuck Norris Joke',
-						url: data.url,
-						thumbnail: {
-							url: data.icon_url,
-						},
-						fields: [{ name: '', value: `${data.value}` }],
-						timestamp: new Date().toISOString(),
-					};
-					interaction.reply({ embeds: [exampleEmbed] });
-				}
-				else {
-					interaction.reply({ content: `Error: ${data.error}, Message: ${data.message}`, ephemaral: true });
-				}
+		try {
+			const response = await fetch('https://api.chucknorris.io/jokes/random');
+			const data = await response.json();
 
+			const exampleEmbed = {
+				color: helper.getRandomColor(),
+				title: 'Chuck Norris Joke',
+				url: data.url,
+				thumbnail: {
+					url: data.icon_url,
+				},
+				fields: [{ name: '', value: `${data.value}` }],
+				timestamp: new Date().toISOString(),
+			};
+			interaction.reply({ embeds: [exampleEmbed] });
 
-			})
-			.catch(error => console.error(error.message));
+		}
+		catch (error) {
+			logger.log('error', error.message);
+			interaction.reply({ content: `Error: ${error}, Status: ${error.status}`, ephemeral: true });
+		}
 
 
 	},

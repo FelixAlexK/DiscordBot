d50df6b2bd12d9065fd1dc2abe1b7adddfdb6b25
Felix <felix.kuhbier@yahoo.com>
2023-01-20 00:56:27 +0100

    added logger

---
+++ logger.js
@@ -0,0 +1,18 @@
+const { format, createLogger, transports } = require('winston');
+const { combine, timestamp, label, prettyPrint, host } = format;
+const CATEGORY = 'winston custom format';
+
+const logger = createLogger({
+	level: 'debug',
+	format: combine(
+		label({ label: CATEGORY }),
+		timestamp({
+			format: 'MMM-DD-YYYY HH:mm:ss',
+		}),
+
+		prettyPrint(),
+	),
+	transports: [new transports.Console()],
+});
+
+module.exports = logger;

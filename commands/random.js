a4728ee1c5e58e6456d0097af090e1fd5682ab8a
Felix <felix.kuhbier@yahoo.com>
2023-01-25 01:06:24 +0100

    removed Unnecessary

--- commands/random.js
+++ commands/random.js
@@ -1,6 +1,6 @@
 const { SlashCommandBuilder } = require('discord.js');
 const _ = require('lodash');
-const wait = require('node:timers/promises').setTimeout;
+
 module.exports = {
 	data: new SlashCommandBuilder()
 		.setName('random')
